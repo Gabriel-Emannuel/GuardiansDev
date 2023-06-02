9f526e8970bee72097b09140cf8447adaaa6ff96
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2023-06-02 13:36:57 -0300

    Fazendo commit

---
+++ ProjetoExpo/src/Descanso.ts
@@ -0,0 +1,13 @@
+class Descanso {
+    private horasDescansadas: number;
+    private semanas: number;
+    private descansado: boolean;
+    private emogi: String;
+
+    constructor() {
+        this.descansado = false;
+        this.horasDescansadas = 0;
+        this.semanas = -1;
+        this.emogi = "";
+    }
+}
